80efb2097feb96a3f1fa7a9d791534ff843036a6
EstefaniaR <a301605@uach.mx>
2022-10-28 12:41:32 -0600

    "Feat: errores"

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+//Se agrego el punto a cada clase, a blog se le quito el signo $
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+/* Se agrego un async para el uso de await */
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //Se le agrego la variable response para que leyera la información
+  console.log(response.data);
+  /* Se cambiaron las comillas por los acentos invertidos */
+  $n.textContent = `${response.name}`;
+  $b.textContent = `${response.blog}`;
+  $l.textContent = `${response.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  //Se agrego el signo de pesos a la n
+  $n.textContent = `Algo salió mal: ${err}`;
 }
 
 displayUser('stolinski').catch(handleError);
